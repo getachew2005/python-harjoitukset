c5531770e3cd7f3abb9ac8f4a3c46e140d100f67
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-08-25 11:51:21 +0300

    Kotitehtävä3.4.py

---
+++ Kotitehtävä3.4.py
@@ -0,0 +1,9 @@
+import math
+
+kokonaisluku = float(input("Mikä on kokonaisluku"))
+kokonaisluku2 = float(input("Mikä on kokonaisluku 2"))
+kokonaisluku3 = float(input("Mikä on kokonaisluku 3"))
+summa = kokonaisluku + kokonaisluku2 + kokonaisluku3
+tulo = kokonaisluku * kokonaisluku2 * kokonaisluku3
+keskiarvo = summa / 3
+print("Kokonaisluku on", kokonaisluku, "Kokonaisluku 2 on", kokonaisluku2, "Kokonaisluku 3 on", kokonaisluku3, "Lukujen summa on", summa, "Lukujen tulo on", tulo, "Lukujen keskiarvo on", keskiarvo)
