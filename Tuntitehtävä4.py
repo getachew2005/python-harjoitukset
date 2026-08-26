216ccd74a49001ed76900d90ece6dafbb3e8f8ef
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-08-26 10:38:29 +0300

    Tuntitehtävä4.py

--- Tuntitehtävä4.py
+++ Tuntitehtävä4.py
@@ -1,3 +1,15 @@
 vuosi = float(input("Anna vuosiluku olympiavuosi: "))
-if vuosi >= 4:
-    print("Annettu vuosiluku olympiavuosi.")
+if vuosi % 4 == 0:
+    print("Oli olympiavuosi")
+else:
+    print("Ei ollut olympiavuosi")
+
+
+
+#toinen tapa
+vuosi = int(input("Annavuosiluku: "))
+
+if vuosi % 4 == 0 and vuosi !=2020
+    print("oli olympiavuosi")
+ else:  
+    print("Ei ollut olympiavuosia")
