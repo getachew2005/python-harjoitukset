4e3e3a251d86320e9927896201acb1d31e709b01
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-08-27 15:15:36 +0300

    Tuntitehtävä6toinenversio.py

---
+++ mod03/Tuntitehtävä6toinenversio.py
@@ -0,0 +1,15 @@
+while True:
+    menu_list = "Select option:\n1. add \n2. substract \n3. multiply \n0. exit"
+    selection = input(menu_list)
+    if sellection == "0":
+        break
+    if selection == "1":
+        print(f"Vastaus: {numero1} + {numero2} = {numero1 + numero2}")
+    elif selection == "2":
+        print(f"Vastaus: {numero1} + {numero2} = {numero1 + numero2}")
+    elif selection == "3":
+        print(f"Vastaus: {numero1} + {numero2} = {numero1 + numero2}")
+        
+    
+
+
