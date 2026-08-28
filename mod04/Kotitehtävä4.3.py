debc0c2721f40d75dc392a10dbba2e34d995b48b
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-08-28 22:21:24 +0300

    Kotitehtävä4.3.py

--- mod04/Kotitehtävä4.3.py
+++ mod04/Kotitehtävä4.3.py
@@ -1,9 +1,18 @@
-arvo =input("Anna biologinen sukupuoli: ")
-if arvo < 117:
-    print("Hemoglobiininarvo on alhainen.")
-elif arvo == 125:
-    print("Hemoglobiininarvo on normaali.")
-elif arvo > 175:
-    print("Hemoglobiininarvo on korkea.")
-else:
-    print(f"Määrä gramma/litra: {gramma} g {litra} l")
+sukupuoli =input("Anna sukupuoli ( nainen/mies): ")
+hemoglobiini = int(input("Anna hemoglobiiniarvo (g/l): "))
+
+if sukupuoli == "nainen":
+    if hemoglobiini < 117:
+        print("Hemoglobiinin arvo on alhainen.")
+    elif hemoglobiini <= 175:
+        print("Hemoglobiiniarvo on normaali.")
+    else:
+        print("Hemoglobiinin arvo on korkea.")
+
+if sukupuoli == "mies":
+    if hemoglobiini < 134:
+        print("Hemoglobiiniarvo on matala.")
+    elif hemoglobiini <= 195:
+        print("Hemoglobiinin arvo on normaali.")
+    else:
+        print("Hemoglobiiniarvo on ylhäinen.")
