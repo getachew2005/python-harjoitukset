89b018c9a1696d4b4cc32542404d09e10d78385d
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-01 18:25:54 +0300

    Kotitehtävä5.2.py

---
+++ mod05/Kotitehtävä5.2.py
@@ -0,0 +1,8 @@
+import math 
+
+tuuma = input("Anna tuumamäärä: ")
+
+while komento != "lopeta":
+    print("Suoritan toiminnon: " + komento)
+    komento = input("Anna komento: ")
+print("Toiminnot lopetettu.")
