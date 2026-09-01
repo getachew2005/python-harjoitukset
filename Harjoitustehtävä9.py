33abe653a994ce722e65146c3d8194c5b1ef7e30
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-01 18:23:59 +0300

    Harjoitustehtävä9.py

---
+++ Harjoitustehtävä9.py
@@ -0,0 +1,4 @@
+nimi = input("Mikä on sinun nimesi")
+ika = input("Mikä on sinun ikäsi")
+syntymapaiva = input("Mikä on sinun syntymäpäiväsi")
+print("Nimesi on", nimi, "Ikäsi on", ika, "Syntymäpäiväsi on", syntymapaiva)
