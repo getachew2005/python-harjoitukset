78a3335c668e5ac94c7dc22724463068aa27cd95
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-03 00:51:23 +0300

    Harjoitustehtävä10.py

--- Harjoitustehtävä10.py
+++ Harjoitustehtävä10.py
@@ -1,5 +1,5 @@
 ika = int(input("Anna ikäsi:"))
 if 15 <= ika < 18:
-    vuosi = float(input("Anna vuosi:"))
+    vuosi = float(input("Syötä vuosi:"))
 if ika >= 18 or (ika >= 15 and vuosi >= 2005):
     print("Voit pelata peliä nimeltään The Incredible Flash.")
