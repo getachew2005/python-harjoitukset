cb265702ea23547ab7c3c21dc7c6a6761e3634d1
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-03 14:03:57 +0300

    Kotitehtävä6.4.py

---
+++ mod06/Kotitehtävä6.4.py
@@ -0,0 +1,8 @@
+Suomen_kaupungit = []
+
+for i in range(5):
+    Suomen_kaupunki = input("Anna jokin kaupungin nimi:")
+    Suomen_kaupungit.append(Suomen_kaupunki)
+
+for Suomen_kaupunki in Suomen_kaupungit:
+    print(Suomen_kaupunki)
