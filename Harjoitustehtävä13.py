7a209e20e37bc32e3a29e5ccd556ca54dbf3afe7
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-05 17:15:22 +0300

    Harjoitustehtävä13.py

---
+++ Harjoitustehtävä13.py
@@ -0,0 +1,7 @@
+kokonaisluku = int(input("Syötä kokonaisluku:"))
+if kokonaisluku > 0:
+    print("Luku on positiivinen.")
+elif kokonaisluku < 0:
+    print("Kokonaisluku on negatiivinen.")
+elif kokonaisluku == 0:
+    print("Luku on nolla.")
