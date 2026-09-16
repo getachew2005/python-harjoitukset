fa8bd9d7a31f839756f8e1264ab0f9dc404a91df
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-16 22:08:45 +0300

    Kotitehtävä9.1.py

--- mod09/Kotitehtävä9.1.py
+++ mod09/Kotitehtävä9.1.py
@@ -4,7 +4,7 @@ class Auto:
         self.huippunopeus = huippunopeus
         self.tämänhetkinen_nopeus = 0
         self.kuljettu_matka = 0
-    def kiidyta(self, muutos):
+    def kiihdyta(self, muutos):
         if self.tamanhetkinen_nopeus - muutos <= 0:
             self.tamanhetkinen_nopeus = 0
         elif self.tamanhetkinen_nopeus + muutos >= self.huippunopeus:
@@ -13,7 +13,7 @@ class Auto:
             self.tamanhetkinen_nopeus += muutos
 
 auto1 = Auto("ABC-123", 142)
-auto1.kiidyta(30)
+auto1.kiihdyta(30)
 auto1.kiihdyta(70)
 auto1.kiihdyta(50)
 print(f"Auton nopeus nyt: {auto1.tamanhetkinen_nopeus}")
@@ -29,4 +29,4 @@ print(f"Auton nopeus nyt: {auto1.tamanhetkinen_nopeus}")
    
 
 auto1 = Auto("ABC-123",142)
-print(f"Rekisteritunnus {auto1.rekisteritunnus} nopeus {auto1.huippunopeus} tämänhetkinen nopeus {auto1.tamanhetkinen_nopeus} kuljettu matka {}")
+print(f"Rekisteritunnus {auto1.rekisteritunnus} nopeus {auto1.huippunopeus} tämänhetkinen nopeus {auto1.tamanhetkinen_nopeus} kuljettu matka {auto1.kuljettu_matka}")
