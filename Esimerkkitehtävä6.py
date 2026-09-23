f8f697c179243283bdf1bd93cf79420b7abf34fe
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-23 20:25:04 +0300

    Esimerkkitehtävä6.py

---
+++ Esimerkkitehtävä6.py
@@ -0,0 +1,14 @@
+pelihahmot = {"Sonic", "Tails", "Knuckles"}
+print(pelihahmot)
+
+pelihahmot.add("Shadow")
+print(pelihahmot)
+
+pelihahmot.remove("Tails")
+print(pelihahmot)
+
+pelihahmot.add("Knuckles")
+print(pelihahmot)
+
+for p in pelihahmot:
+    print(p)
