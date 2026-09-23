089abc33ceb788065f027772f30b2baa36eaca87
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-24 00:25:38 +0300

    Harjoitustehtävä47.py

---
+++ Harjoitustehtävä47.py
@@ -0,0 +1,9 @@
+henkilo = {
+    "nimi": "Matti",
+    "ika": 20,
+    "kaupunki": "Helsinki"
+}
+
+print("Henkilön nimi: ", henkilo["nimi"])
+print("Henkilön ikä: ", henkilo["ika"])
+print("Kotikaupunki: ", henkilo["kaupunki"])
