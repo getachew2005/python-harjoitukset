e850c0c2a87a34550b62dd25cb7a6d19b48152e9
Liul Getachew <Liul.Getachew@metropolia.fi>
2026-09-24 11:19:12 +0300

    Harjoitustehtävä48.py

---
+++ Harjoitustehtävä48.py
@@ -0,0 +1,19 @@
+class Työntekijä:
+
+    työntekijöiden_lukumäärä = 0
+
+    def __init__(self, etunimi, sukunimi):
+        Työntekijä.työntekijöiden_lukumäärä = Työntekijä.työntekijöiden_lukumäärä + 1
+        self.työntekijänumero = Työntekijä.työntekijöiden_lukumäärä
+        self.etunimi = etunimi
+        self.sukunimi = sukunimi
+
+    def tulosta_tiedot(self):
+        print(f"{self.työntekijänumero}: {self.etunimi} {self.sukunimi}")
+
+työntekijät = []
+työntekijät.append(Työntekijä("Tony", "Stark"))
+työntekijät.append(Työntekijä("Steve", "Rogers"))
+
+for t in työntekijät:
+    t.tulosta_tiedot()
